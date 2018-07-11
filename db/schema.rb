d8396e18599b16227da71fb1db87fd755c7d16a5
theshawe <david@invisiblebuilding.com>
2018-07-11 23:26:53 +0100

    ready for net

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160206181531) do
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "post_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
-
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
